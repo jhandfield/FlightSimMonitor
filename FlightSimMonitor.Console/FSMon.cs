d4f5bb2fca22b0f2df80c4af34552df73e6fe3a2
Justin Handfield <justin@handfield.org>
2021-03-01 03:19:42 -0500

    A little cleanup

--- FlightSimMonitor.Console/FSMon.cs
+++ FlightSimMonitor.Console/FSMon.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Threading;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Handfield.FlightSimMonitor;
 
 namespace Handfield.FlightSimMonitor.Console
 {
