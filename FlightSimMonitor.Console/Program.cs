0aabc0e125c7c8b51f70cfbe2209aa7269911b8c
Justin Handfield <justin@handfield.org>
2021-03-01 02:54:43 -0500

    Renamed the project yet again, implemented basic SimConnect functionality, built a simple console example/test app to accompany the library.

---
+++ FlightSimMonitor.Console/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Handfield.FlightSimMonitor;
+
+namespace Handfield.FlightSimMonitor.Console
+{
+    class FSMon
+    {
+        private static FlightSimMonitor _fsMon;
+        private static string _host = "127.0.0.1";
+        private static uint _port = 500;
+        private static bool _run;
+        private static bool _isConnecting;
+
+        private static string StatusLine
+        {
+            get
+            {
+                if (_isConnecting)
+                    return $"CONNECTING to {_host}:{_port}";
+                else
+                    return (_fsMon.IsConnected) ? $"CONNECTED to {_fsMon.Hostname}:{_fsMon.Port}" : "DISCONNECTED";
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Initialize
+            _run = true;
+            _isConnecting = false;
+            _fsMon = new FlightSimMonitor();
+            _fsMon.Connected += _fsMon_Connected;
+            _fsMon.Disconnected += _fsMon_Disconnected;
+
+            // Drop into the menu loop
+            DoMenu();
+
+            // Main loop
+            while (_run) { }
+
+            // End
+            System.Console.Write("Press any key to exit...");
+            System.Console.ReadKey();
+        }
+
+        private static void DoMenu()
+        {
+            // Clear the screen
+            System.Console.Clear();
+
+            // Update the connection status line at the bottom of the window
+            UpdateStatus();
+
+            // Display the menu
+            System.Console.Write("1) Connect\n2) Quit\n> ");
+
+            // Collect user input
+            ConsoleKeyInfo userSel = System.Console.ReadKey();
+            System.Console.Write("\n");
+
+            switch(userSel.KeyChar)
+            {
+                case '1':
+                    // Prompt for and set a value in _host
+                    GetHostname();
+
+                    // Prompt for and set a value in _port
+                    GetPort();
+
+                    // Tell the user what's happening
+                    System.Console.WriteLine($"Connecting to host {_host} on port {_port}...");
+                    _isConnecting = true;
+                    UpdateStatus();
+
+                    // Connect to SimConnect
+                    try
+                    {
+                        _fsMon.Connect("FlightSimMonitor Console", _host, _port);
+                    }
+                    catch (Exception e)
+                    {
+                        System.Console.WriteLine($"Failed to connect to host {_host} on port {_port}!");
+                        _isConnecting = false;
+                        UpdateStatus();
+                        Thread.Sleep(4000);
+                        DoMenu();
+                    }
+                    break;
+                case '2':
+                    Environment.Exit(0);
+                    break;
+                default:
+                    DoMenu();
+                    break;
+            }
+        }
+
+        private static void GetPort()
+        {
+            System.Console.Write($"Port [{_port}]: ");
+            string sPort = System.Console.ReadLine();
+
+            // If we got input, check that it can be parsed as a uint; if no input, we keep the original value
+            if (!String.IsNullOrWhiteSpace(sPort) && uint.TryParse(sPort, out uint port))
+            {
+                // Check that the port value is valid
+                if (port < 1 || port > 65535)
+                {
+                    // Display an error message and re-prompt
+                    System.Console.WriteLine("Port values must be between 1 and 65535");
+                    GetPort();
+                }
+                else
+                {
+                    // User provided input, it's a number, and it's in the right range - reset _port
+                    _port = port;
+                }
+            }
+        }
+        private static void GetHostname()
+        {
+            System.Console.Write($"Host [{_host}]: ");
+            string hostName = System.Console.ReadLine();
+
+            // If the user entered something, reset _host; otherwise, do nothing.
+            if (!String.IsNullOrWhiteSpace(hostName))
+                _host = hostName;
+        }
+
+        private static void _fsMon_Disconnected(object sender, FlightSimMonitor.DisconnectedEventArgs e)
+        {
+            UpdateStatus();
+            System.Console.WriteLine($"SimConnect disconnected at {e.DisconnectedTime.ToString()}! Last connected at {e.LastConnectedTime.ToString()}");
+        }
+
+        private static void _fsMon_Connected(object sender, FlightSimMonitor.ConnectedEventArgs e)
+        {
+            // We're no longer connecting, we're connected
+            _isConnecting = false;
+            UpdateStatus();
+            System.Console.WriteLine($"SimConnect connected at {e.ConnectedTime.ToString()}! Last disconnected at {e.LastDisconnectedTime.ToString()}");
+        }
+
+        private static void UpdateStatus()
+        {
+            // Get the current cursor position so we can move back when we're done
+            int curLeft = System.Console.CursorLeft;
+            int curTop = System.Console.CursorTop;
+
+            // Move to the bottom line and update
+            System.Console.SetCursorPosition(0, System.Console.WindowHeight - 1);
+            System.Console.Write(new string(' ', System.Console.WindowWidth - 1));
+            System.Console.SetCursorPosition(0, System.Console.WindowHeight - 1);
+            System.Console.Write($"Status: {StatusLine}");
+
+            // Move cursor back to its previous position
+            System.Console.SetCursorPosition(curLeft, curTop);
+        }
+    }
+}
---
+++ FlightSimMonitor/FlightSimMonitor.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CTrue.FsConnect;
+
+namespace Handfield.FlightSimMonitor
+{
+    public class FlightSimMonitor
+    {
+        #region Fields
+        private FsConnect _fsConn;
+        private DateTime _lastConnectedTime;
+        private DateTime _lastDisconnecedTime;
+        private uint _port;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Hostname of SimConnect server to connect to
+        /// </summary>
+        public string Hostname { get; set; }
+
+        /// <summary>
+        /// Port number of SimConnect server to connect to
+        /// </summary>
+        public uint Port
+        {
+            get
+            {
+                return _port;
+            }
+            set
+            {
+                if (value < 1 || value > 65535)
+                    throw new ArgumentOutOfRangeException("An invalid value was passed - must be between 1 and 65535");
+                else
+                    _port = value;
+            }
+        }
+
+        /// <summary>
+        /// Name to identify to SimConnect as
+        /// </summary>
+        public string ApplicationName { get; set; }
+
+        /// <summary>
+        /// Indicates whether SimConnect is currently connected
+        /// </summary>
+        public bool IsConnected { get { return _fsConn.Connected; } }
+        #endregion
+
+        #region Constructors
+        public FlightSimMonitor()
+        {
+            _fsConn = new FsConnect();
+            _fsConn.ConnectionChanged += _fsConn_ConnectionChanged;
+
+            _lastConnectedTime = DateTime.MinValue;
+            _lastDisconnecedTime = DateTime.MinValue;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Attempt to connect to SimConnect with the currently-configured parameters
+        /// </summary>
+        public void Connect()
+        {
+            _fsConn.Connect(ApplicationName, Hostname, Port, SimConnectProtocol.Ipv4);
+        }
+
+        /// <summary>
+        /// Set SimConnect connection parameters, then attempt to connect
+        /// </summary>
+        /// <param name="applicationName">The name of your application, to identify to SimConnect</param>
+        /// <param name="hostName">Hostname to connect to SimConnect at</param>
+        /// <param name="port">Port number to connect to SimConnect</param>
+        public void Connect(string applicationName, string hostName, uint port)
+        {
+            // Set properties based on input
+            ApplicationName = applicationName;
+            Hostname = hostName;
+            Port = port;
+
+            // Call Connect()
+            Connect();
+        }
+
+        /// <summary>
+        /// Handle ConnectionChanged events from FsConnect
+        /// </summary>
+        private void _fsConn_ConnectionChanged(object sender, EventArgs e)
+        {
+            // Fire the appropriate event, based on the new connection state
+            if (_fsConn.Connected)
+            {
+                _lastConnectedTime = DateTime.Now;
+                OnConnected(new ConnectedEventArgs { ConnectedTime = _lastConnectedTime, LastDisconnectedTime = _lastDisconnecedTime });
+            }
+            else
+            {
+                _lastDisconnecedTime = DateTime.Now;
+                OnDisconnected(new DisconnectedEventArgs { DisconnectedTime = _lastDisconnecedTime, LastConnectedTime = _lastConnectedTime });
+            }
+
+        }
+        #endregion
+
+        #region Event Definitions
+        /// <summary>
+        /// Fires when SimConnect reports it has connected to the server
+        /// </summary>
+        public event EventHandler<ConnectedEventArgs> Connected;
+
+        /// <summary>
+        /// Fires when SimConnect reports it has been disconnected from the server
+        /// </summary>
+        public event EventHandler<DisconnectedEventArgs> Disconnected;
+        #endregion
+
+        #region Event Handlers
+        protected virtual void OnConnected(ConnectedEventArgs e)
+        {
+            EventHandler<ConnectedEventArgs> raiseEvent = Connected;
+
+            if (raiseEvent != null)
+            {
+                // Set properties
+                e.ConnectedTime = _lastConnectedTime;
+                e.LastDisconnectedTime = _lastDisconnecedTime;
+
+                // Raise the event
+                raiseEvent(this, e);
+            }
+        }
+
+        protected virtual void OnDisconnected(DisconnectedEventArgs e)
+        {
+            EventHandler<DisconnectedEventArgs> raiseEvent = Disconnected;
+
+            if (raiseEvent != null)
+            {
+                // Set properties
+                e.DisconnectedTime = _lastDisconnecedTime;
+                e.LastConnectedTime = _lastConnectedTime;
+
+                // Raise the event
+                raiseEvent(this, e);
+            }
+        }
+        #endregion
+
+        #region Event Arguments
+        public class ConnectedEventArgs
+        {
+            public DateTime ConnectedTime { get; set; }
+            public DateTime LastDisconnectedTime { get; set; }
+
+            public ConnectedEventArgs() { }
+            public ConnectedEventArgs(DateTime connTime, DateTime disconTime)
+            {
+                ConnectedTime = connTime;
+                LastDisconnectedTime = disconTime;
+            }
+        }
+
+        public class DisconnectedEventArgs
+        {
+            public DateTime DisconnectedTime { get; set; }
+            public DateTime LastConnectedTime { get; set; }
+
+            public DisconnectedEventArgs() { }
+            public DisconnectedEventArgs(DateTime disconnTime, DateTime connTime)
+            {
+                DisconnectedTime = disconnTime;
+                LastConnectedTime = connTime;
+            }
+        }
+        #endregion
+    }
+}
